94238d6f0a79f1be46e678724c0dde9d0c2bf7fc
johnkiprop <kipropjohn14@gmail.com>
2022-12-26 19:05:47 +0300

    Reactive Springboot 101 Revision

---
+++ movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
@@ -0,0 +1,99 @@
+package com.reactivespring.repository;
+
+import com.reactivespring.domain.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+@ActiveProfiles("test")
+@ImportAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
+class MovieInfoRepositoryIntegrationTest {
+@Autowired MovieInfoRepository movieInfoRepository;
+
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+        movieInfoRepository.saveAll(movieInfos).blockLast();
+        //blockLast() ensures the save is completed before proceeding to test, coz of non-blocking call
+        //only use in testing though
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll(){
+    var moviesInfoFlux = movieInfoRepository.findAll().log();
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+    @Test
+    void findById(){
+        var moviesInfoMono = movieInfoRepository.findById("abc").log();
+        StepVerifier.create(moviesInfoMono)
+                //.expectNextCount(1)
+                .assertNext(movieInfo -> {
+                    assertEquals("Dark Knight Rises", movieInfo.getName());
+                })
+                .verifyComplete();
+    }
+    @Test
+    void saveMovieInfo(){
+        //given
+        var movieInfo = new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+        //then
+        var moviesInfoMono = movieInfoRepository.save(movieInfo).log();
+
+        StepVerifier.create(moviesInfoMono)
+                //.expectNextCount(1)
+                .assertNext(movieInfo1 -> {
+                    assertNotNull(movieInfo1.getMovieInfoId());
+                    assertEquals("Batman Begins", movieInfo1.getName());
+                })
+                .verifyComplete();
+    }
+    @Test
+    void updateMovieInfo(){
+        //given
+        var movieInfo = movieInfoRepository.findById("abc").block();
+        movieInfo.setYear(2021);
+        //then
+        var moviesInfoMono = movieInfoRepository.save(movieInfo).log();
+
+        StepVerifier.create(moviesInfoMono)
+                //.expectNextCount(1)
+                .assertNext(movieInfo1 -> {
+                    assertEquals(2021, movieInfo1.getYear());
+                })
+                .verifyComplete();
+    }
+    @Test
+    void deleteMovieInfo(){
+        movieInfoRepository.deleteById("abc").block();
+        var moviesInfoFlux = movieInfoRepository.findAll().log();
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+}
